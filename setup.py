26320d0d28c478d6c15e0cd20ebcca932792f538
KNPOB <qilong-kirov.liu@outlook.com>
2023-10-18 16:49:46 +0800

    remove long description in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 from setuptools import setup
 
-with open("README.md", 'r') as f:
-    long_description = f.read()
-
 setup(
 	name='mesh4d',
 	version='0.0',
 	description='Toolkit for 4D (3D + T) data visualisation, operation, and dynamic dense-registration. Extents the existing concepts of 3D mesh toolkit to a 4D mesh toolkit.',
-    long_description=long_description,
     url='',
     license='',
 	author='Qilong Liu',
